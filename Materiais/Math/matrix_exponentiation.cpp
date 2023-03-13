2e980f5ee0edd9c3444411aef43d3db1a3a8e5ef
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-03-13 16:35:56 -0300

    Adds matrix exponetiation

--- Materiais/Math/matrix_exponentiation.cpp
+++ Materiais/Math/matrix_exponentiation.cpp
@@ -1,95 +1,105 @@
-#include <bits/stdc++.h>
-#define debug(x) cout << "[" << #x << " = " << x << "] "
-#define ff first
-#define ss second
+// Description:
+// Calculate the nth term of a linear recursion
 
-using namespace std;
+// Example Fibonacci:
+// Given a linear recurrence, for example fibonacci
+// F(n) = n, x <= 1
+// F(n) = F(n - 1) + F(n - 2), x > 1
+
+// The recurrence has two terms, so we can build a matrix 2 x 1 so that
+// n + 1 = transition * n
+
+// (2 x 1) = (2 x 2) * (2 x 1)
+// F(n)      = a b * F(n - 1)
+// F(n - 1)    c d   F(n - 2)
+
+// Another Example:
+// Given a grid 3 x n, you want to color it using 3 distinct colors so that
+// no adjacent place has the same color. In how many different ways can you do that?
+// There are 6 ways for the first column to be colored using 3 distinct colors
+// ans 6 ways using 2 equal colors and 1 distinct one
+
+// Adding another column, there are:
+// 3 ways to go from 2 equal to 2 equal
+// 2 ways to go from 2 equal to 3 distinct
+// 2 ways to go from 3 distinct to 2 equal
+// 2 ways to go from 3 distinct to 3 distinct
+
+// So we star with matrix 6 6 and multiply it by the transition 3 2 and get 18 12
+//                        6 6                                   2 2         12 12
+// the we can exponentiate this matrix to find the nth column
+
+// Problem:
+// https://cses.fi/problemset/task/1722/
+
+// Complexity:
+// O(log n)
+
+// How to use:
+// vector<vector<ll>> v = {{1, 1}, {1, 0}};
+// Matriz transition = Matriz(v);
+// cout << fexp(transition, n)[0][1] << '\n';
+
 using ll = long long;
-using ld = long double;
-using pii = pair<int,int>;
-using vi = vector<int>;
-
-using tii = tuple<int,int,int>;
-
-const int oo = (int)1e9;
-const ll OO = 0x3f3f3f3f3f3f3f3fLL;
-
+ 
 const int MOD = 1e9+7;
  
-struct Mat{
-    vector<vector<ll>> matriz;
-    int l, c;
+struct Matriz{
+    vector<vector<ll>> mat;
+    int rows, columns;
+    
+    vector<ll> operator[](int i){
+        return mat[i];
+    }
  
-    Mat(vector<vector<ll>>& mat){
-        matriz = mat;
-        l = mat.size();
-        c = mat[0].size();
+    Matriz(vector<vector<ll>>& matriz){
+        mat = matriz;
+        rows = mat.size();
+        columns = mat[0].size();
     }
     
-    Mat(int r, int col, bool identidade=false){ 
-        l = r;  c = col;
-        matriz.assign(l, vector<ll>(col, 0));
-        if(identidade){
-            for(int i = 0; i < min(l,col); i++)
-                matriz[i][i] = 1;
+    Matriz(int row, int column, bool identity=false){ 
+        rows = row;  columns = column;
+        mat.assign(rows, vector<ll>(columns, 0));
+        if(identity) {
+            for(int i = 0; i < min(rows,columns); i++) {
+                mat[i][i] = 1;
+            }
         }
     }
  
-    Mat operator * (const Mat& a) const{
-        assert(c == a.l);
-        vector<vector<ll>> resp(l, vector<ll>(a.c, 0));
-
-        for(int i = 0; i < l; i++){
-            for(int j = 0; j < a.c; j++){
-                for(int k = 0; k < a.l; k++){
-                    resp[i][j] = (resp[i][j] + (matriz[i][k]*a.matriz[k][j]) % MOD) % MOD;
+    Matriz operator * (Matriz a) {
+        assert(columns == a.rows);
+        vector<vector<ll>> resp(rows, vector<ll>(a.columns, 0));
+ 
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < a.columns; j++){
+                for(int k = 0; k < a.rows; k++){
+                    resp[i][j] = (resp[i][j] + (mat[i][k] * 1LL * a[k][j]) % MOD) % MOD;
                 }
             }
         }
-        return Mat(resp);
+        return Matriz(resp);
     }
  
-    Mat operator + (const Mat& a) const{
-        assert(l == a.l && c == a.c); 
-        vector<vector<ll>> resp(l, vector<ll>(c,0));
-        for(int i = 0; i < l; i++){
-            for(int j = 0; j < c; j++){
-                resp[i][j] = (resp[i][j] + matriz[i][j] + a.matriz[i][j]) % MOD;
+    Matriz operator + (Matriz a) {
+        assert(rows == a.rows && columns == a.columns); 
+        vector<vector<ll>> resp(rows, vector<ll>(columns,0));
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                resp[i][j] = (resp[i][j] + mat[i][j] + a[i][j]) % MOD;
             }
         }
-        return Mat(resp);
+        return Matriz(resp);
     }
 };
  
-Mat fexp(Mat& base, ll expoente, ll sz){
-    Mat result = Mat(sz, sz, 1);
-    while(expoente > 0){
-        if(expoente & 1) result = result * base;
+Matriz fexp(Matriz base, ll exponent){
+    Matriz result = Matriz(base.rows, base.rows, 1);
+    while(exponent > 0){
+        if(exponent & 1LL) result = result * base;
         base = base * base;
-        expoente /= 2;
+        exponent  = exponent >> 1;
     }
     return result;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    ll n, a, b;
-    cin >> a >> b >> n;
-
-    Mat X(2,2);
-    
-    X.matriz[0][1] = 1;
-    X.matriz[1][0] = -1;
-    X.matriz[1][1] = 1;
-
-    Mat y = fexp(X,n-1,2);
-
-    ll ans = y.matriz[0][0] * a + y.matriz[0][1] * b;
-
-    while(ans < 0)
-        ans += MOD;
-
-    cout << ans % MOD << endl;
 }
