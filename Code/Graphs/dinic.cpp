650a679e723844393e81b3b766271e0140c69649
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-05-13 22:17:46 -0300

    Adds flow algorithms

--- Code/Graphs/dinic.cpp
+++ Code/Graphs/dinic.cpp
@@ -1,77 +1,103 @@
-const int N = 300;
+// Description: 
+// Obtains the maximum possible flow rate given a network. A network is a graph with a single source vertex and a single sink vertex in which each edge has a capacity
+
+// Problem:
+// https://codeforces.com/gym/103708/problem/J
+
+// Complexity:
+// O(V^2 * E) where V is the number of vertex and E is the number of edges
+
+// Unit network
+// A unit network is a network in which for any vertex except source and sink either incoming or outgoing edge is unique and has unit capacity (matching problem).
+// Complexity on unit networks: O(E * sqrt(V))
+
+// Unity capacity networks
+// A more generic settings when all edges have unit capacities, but the number of incoming and outgoing edges is unbounded
+// Complexity on unity capacity networks: O(E * sqrt(E))
+
+// How to use:
+// Dinic dinic = Dinic(num_vertex, source, sink);
+// dinic.add_edge(vertex1, vertex2, capacity);
+// cout << dinic.flow() << '\n';
+
+struct FlowEdge {
+    int v, u;
+    long long cap, flow = 0;
+    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+};
 
 struct Dinic {
-    struct Edge{
-        int from, to; ll flow, cap;
-    };
-    vector<Edge> edge;
+    const long long flow_inf = 1e18;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj;
+    int n, m = 0;
+    int s, t;
+    vector<int> level, ptr;
+    queue<int> q;
 
-    vector<int> g[N];
-    int ne = 0;
-    int lvl[N], vis[N], pass;
-    int qu[N], px[N], qt;
-
-    ll run(int s, int sink, ll minE) {
-        if(s == sink) return minE;
-
-        ll ans = 0;
-
-        for(; px[s] < (int)g[s].size(); px[s]++) {
-            int e = g[s][ px[s] ];
-            auto &v = edge[e], &rev = edge[e^1];
-            if(lvl[v.to] != lvl[s]+1 || v.flow >= v.cap)
-                continue;           // v.cap - v.flow < lim
-            ll tmp = run(v.to, sink,min(minE, v.cap-v.flow));
-            v.flow += tmp, rev.flow -= tmp;
-            ans += tmp, minE -= tmp;
-            if(minE == 0) break;
-        }
-        return ans;
+    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
     }
-    bool bfs(int source, int sink) {
-        qt = 0;
-        qu[qt++] = source;
-        lvl[source] = 1;
-        vis[source] = ++pass;
-        for(int i = 0; i < qt; i++) {
-            int u = qu[i];
-            px[u] = 0;
-            if(u == sink) return true;
-            for(auto& ed : g[u]) {
-                auto v = edge[ed];
-                if(v.flow >= v.cap || vis[v.to] == pass)
-                    continue; // v.cap - v.flow < lim
-                vis[v.to] = pass;
-                lvl[v.to] = lvl[u]+1;
-                qu[qt++] = v.to;
+
+    void add_edge(int v, int u, long long cap) {
+        edges.emplace_back(v, u, cap);
+        edges.emplace_back(u, v, 0);
+        adj[v].push_back(m);
+        adj[u].push_back(m + 1);
+        m += 2;
+    }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int id : adj[v]) {
+                if (edges[id].cap - edges[id].flow < 1)
+                    continue;
+                if (level[edges[id].u] != -1)
+                    continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
             }
         }
-        return false;
+        return level[t] != -1;
     }
-    ll flow(int source, int sink) {
-        reset_flow();
-        ll ans = 0;
-        //for(lim = (1LL << 62); lim >= 1; lim /= 2)
-        while(bfs(source, sink))
-            ans += run(source, sink, LLINF);
-        return ans;
-    }
-    void addEdge(int u, int v, ll c, ll rc) {
-        Edge e = {u, v, 0, c};
-        edge.pb(e);
-        g[u].push_back(ne++);
 
-        e = {v, u, 0, rc};
-        edge.pb(e);
-        g[v].push_back(ne++);
+    long long dfs(int v, long long pushed) {
+        if (pushed == 0)
+            return 0;
+        if (v == t)
+            return pushed;
+        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+            int id = adj[v][cid];
+            int u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+                continue;
+            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0)
+                continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
     }
-    void reset_flow() {
-        for(int i = 0; i < ne; i++)
-            edge[i].flow = 0;
-        memset(lvl, 0, sizeof(lvl));
-        memset(vis, 0, sizeof(vis));
-        memset(qu, 0, sizeof(qu));
-        memset(px, 0, sizeof(px));
-        qt = 0; pass = 0;
+
+    long long flow() {
+        long long f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs())
+                break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(s, flow_inf)) {
+                f += pushed;
+            }
+        }
+        return f;
     }
 };
