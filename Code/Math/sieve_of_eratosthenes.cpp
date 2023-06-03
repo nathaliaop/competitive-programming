333184fd605c74ea557045658909554813fda686
Nathália Pereira <80282316+nathaliaop@users.noreply.github.com>
2023-06-03 15:31:14 -0300

    Update sieve_of_eratosthenes.cpp

--- Code/Math/sieve_of_eratosthenes.cpp
+++ Code/Math/sieve_of_eratosthenes.cpp
@@ -1,9 +1,14 @@
-int n;
-vector<bool> is_prime(n+1, true);
-is_prime[0] = is_prime[1] = false;
-for (int i = 2; i <= n; i++) {
-    if (is_prime[i] && (long long)i * i <= n) {
-        for (int j = i * i; j <= n; j += i)
-            is_prime[j] = false;
+vector<bool> is_prime(MAX, true);
+vector<int> primes;
+
+void sieve() {
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i < MAX; i++) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+
+            for (int j = i + i; j < MAX; j += i)
+                is_prime[j] = false;
+        }
     }
 }
