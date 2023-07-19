783b0e4d56d301559081b0cf139d950757c2671f
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-19 10:25:59 -0300

    Adds algorithm to find all subsets

---
+++ Code/Algorithms/subsets.cpp
@@ -0,0 +1,13 @@
+void subsets(vector<int>& nums){
+  int n = nums.size();
+  int powSize = 1 << n;
+
+  for(int counter = 0; counter < powSize; counter++){
+    for(int j = 0; j < n; j++){
+      if((counter & (1LL << j)) != 0) {
+        cout << nums[j] << ' ';
+      }  
+    }
+    cout << '\n';
+  }
+}
