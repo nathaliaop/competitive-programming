372cf4020dc116ad45e15102fa756bfc70b50d64
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-19 16:59:51 -0300

    Modifies algprithm to convert from another base to base 10

--- Code/Math/to_decimal.cpp
+++ Code/Math/to_decimal.cpp
@@ -1,9 +1,9 @@
-const string digits { "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
-
 long long to_decimal(const string& rep, long long base) {
   long long n = 0;
 
   for (auto c : rep) {
+    // if the number can't be represented in this base
+    if (c > digits[base - 1]) return -1;
     n *= base;
     n += digits.find(c);
   }
